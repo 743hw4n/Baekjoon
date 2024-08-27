f61213fc468cc3d63027a72cedbb08a9422bbe7c
ta2hwan <80195853+ta2hwan@users.noreply.github.com>
2024-08-27 13:24:18 +0900

    [Silver IV] Title: 큐, Time: 204 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	int n;
+	string cmd;
+
+	cin >> n;
+
+	queue<int> q;
+
+	while (n--) {
+		cin >> cmd;
+
+		if (cmd == "push") {
+			int x;
+			cin >> x;
+			q.push(x);
+		}
+
+		else if (cmd == "pop") {
+			if (q.empty()) cout << -1 << '\n';
+
+			else {
+				cout << q.front() << '\n';
+				q.pop();
+			}
+		}
+
+		else if (cmd == "size") {
+			cout << q.size() << '\n';
+		}
+
+		else if (cmd == "empty") {
+			if (q.empty()) cout << 1 << '\n';
+			else cout << 0 << '\n';
+		}
+
+		else if (cmd == "front") {
+			if (q.empty()) cout << -1 << '\n';
+			else cout << q.front() << '\n';
+		}
+
+		else if (cmd == "back") {
+			if (q.empty()) cout << -1 << '\n';
+			else cout << q.back() << '\n';
+		}
+	}
+
+
+	return 0;
+}
