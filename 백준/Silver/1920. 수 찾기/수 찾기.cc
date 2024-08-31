a107c8e4fd0fb061125dfce5b28b0517f4e188a9
ta2hwan <80195853+ta2hwan@users.noreply.github.com>
2024-08-31 12:53:22 +0900

    [Silver IV] Title: 수 찾기, Time: 44 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, m;
+int arr[100000];
+
+
+void binary_search(int x) {
+	int left = 0, mid = 0, right = n - 1;
+	
+	while (left <= right) {
+		mid = (left + right) / 2;
+
+		if (arr[mid] == x) {
+			cout << 1 << '\n';
+			return;
+		}
+		else if (arr[mid] > x)
+			right = mid - 1;
+		else
+			left = mid + 1;
+	}
+
+	cout << 0 << '\n';
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + n);
+
+	cin >> m;
+
+	int x;
+
+	for (int i = 0; i < m; i++)
+	{
+		cin >> x;
+		binary_search(x);
+	}
+
+	return 0;
+}
