00593ee4ee428d3a99e10019abd39c70382f9562
ta2hwan <80195853+ta2hwan@users.noreply.github.com>
2024-11-22 11:18:33 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+  int T;
+  cin >> T;
+
+  while(T--) {
+    string s;
+    cin >> s;
+    cout << s[0] << s[s.size() -1] << "\n";
+  }
+
+
+  return 0;
+}
